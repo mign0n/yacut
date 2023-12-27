9b6b5f60c8dacc4c41f695e4ad7419cb47fd43d8
mign0n <mign0n.git@gmail.com>
2023-12-27 13:45:57 +0500

    refactor: Optimize settings.

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'SUP$R-SECR3T-KEY')
 
     DEFAULT_SHORT_ID_SIZE = 6
     ORIGINAL_LINK_MAX_SIZE = 256
