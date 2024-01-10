7415a57b1f6cd97bd5eea845eecc648bb94b31e3
mign0n <mign0n.git@gmail.com>
2024-01-10 16:26:20 +0500

    refactor: Add validator by valid characters.

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, URLField
-from wtforms.validators import URL, DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
 
 from yacut import app
 
 CREATE = 'Создать'
 CUSTOM_SHORT_LINK = 'Ваш вариант короткой ссылки'
+INVALID_SHORT_LINK_NAME = 'Указано недопустимое имя для короткой ссылки'
 LONG_LINK = 'Длинная ссылка'
 REQUIRED_FIELD = 'Обязательное поле'
 
@@ -15,12 +16,19 @@ class URLMapForm(FlaskForm):
         LONG_LINK,
         validators=[
             DataRequired(message=REQUIRED_FIELD),
-            Length(1, app.config['ORIGINAL_LINK_MAX_SIZE']),
+            Length(max=app.config['ORIGINAL_LINK_MAX_SIZE']),
             URL(),
         ],
     )
     custom_id = URLField(
         CUSTOM_SHORT_LINK,
-        validators=[Length(1, app.config['SHORT_ID_MAX_SIZE']), Optional()],
+        validators=[
+            Length(max=app.config['SHORT_ID_MAX_SIZE']),
+            Optional(),
+            Regexp(
+                app.config['SHORT_ID_PATTERN'],
+                message=INVALID_SHORT_LINK_NAME,
+            ),
+        ],
     )
     submit = SubmitField(CREATE)
