aeac94ef8947c4051edb7339ec59960cd90c67d3
mign0n <mign0n.git@gmail.com>
2024-01-18 22:32:03 +0500

    fix: Setup data for API testing.

--- postman_collection/set_up_data.sh
+++ postman_collection/set_up_data.sh
@@ -8,7 +8,10 @@ esac
 
 cd ../
 echo "from yacut import db; \
+     from yacut.models import URLMap; \
      db.create_all(); \
      URLMap.query.delete(); \
-     url_map_object = URLMap(original='https://example.com/', short='example'); db.session.add(url_map_object); db.session.commit()" | flask shell >/dev/null 2>&1
+     url_map_object = URLMap(original='https://example.com/', short='example'); \
+     db.session.add(url_map_object); \
+     db.session.commit()" | flask shell >/dev/null 2>&1
 cd -
